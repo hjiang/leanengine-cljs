5d1a6598f884ad42977a046390f043ffa6861630
Hong Jiang <hjiang@leancloud.rocks>
2016-05-20 18:55:18 +0800

    Get things working on Node.

--- scripts/build.clj
+++ scripts/build.clj
@@ -5,9 +5,8 @@
 (let [start (System/nanoTime)]
   (b/build "src"
     {:main 'leanengine-cljs.core
+     :target :nodejs
      :output-to "out/leanengine_cljs.js"
      :output-dir "out"
      :verbose true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
-
-
--- src/leanengine_cljs/core.cljs
+++ src/leanengine_cljs/core.cljs
@@ -1,9 +1,14 @@
 (ns leanengine-cljs.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [cljs.nodejs :as nodejs]))
 
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
 
-(enable-console-print!)
+(nodejs/enable-util-print!)
 
-(println "Hello world!")
+;; (println "Hello world!")
+
+(defn -main [& args]
+  (println "Hello world!"))
+
+(set! *main-cli-fn* -main)

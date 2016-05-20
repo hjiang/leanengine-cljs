8a87cdf26a51458372bf773528f8c96aab4499ff
Hong Jiang <hjiang@leancloud.rocks>
2016-05-20 19:50:43 +0800

    Minimal web server.

--- src/leanengine_cljs/core.cljs
+++ src/leanengine_cljs/core.cljs
@@ -4,11 +4,22 @@
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
 
+(def leanengine (nodejs/require "leanengine"))
+(def express (nodejs/require "express"))
+
 (nodejs/enable-util-print!)
 
-;; (println "Hello world!")
-
 (defn -main [& args]
-  (println "Hello world!"))
+  (.initialize leanengine
+               (-> process (.-env) (.-LC_APP_ID))
+               (-> process (.-env) (.-LC_APP_KEY))
+               (-> process (.-env) (.-LC_APP_MASTER_KEY)))
+  (-> leanengine (.Cloud) (.useMasterKey))
+  ;; initialize app
+  (let [port (parseInt (or (-> process (.-env) (.-LC_APP_PORT)) 3000))
+        app (express)]
+    (.use app (.-Cloud leanengine))
+    (.get app "/" (fn [req res] (.send res "Hello LeanEngine!")))
+    (.listen app port (fn [] (println "App is running!")))))
 
 (set! *main-cli-fn* -main)
